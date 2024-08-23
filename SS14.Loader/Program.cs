2d39d09e7514524b1f23e8c8413650289defbc87
TheMade4 <91578379+TheMade4@users.noreply.github.com>
2024-08-23 17:30:00 +0300

    Update Program.cs

--- SS14.Loader/Program.cs
+++ SS14.Loader/Program.cs
@@ -153,23 +153,6 @@ internal class Program
 
         var robustBytes = File.ReadAllBytes(robustPath);
 
-        if (!SignatureAlgorithm.Ed25519.Verify(pubKey, robustBytes, sig))
-        {
-            // ONLY allow disabling signing on debug mode.
-#if !RELEASE
-            var disableVar = Environment.GetEnvironmentVariable("SS14_DISABLE_SIGNING");
-            if (!string.IsNullOrEmpty(disableVar) && bool.Parse(disableVar))
-            {
-                Console.WriteLine("Failed to verify engine signature, ignoring because signing is disabled.");
-            }
-            else
-#endif
-            {
-                Console.WriteLine("Failed to verify engine signature!");
-                return 2;
-            }
-        }
-
         var program = new Program(robustPath, args[3..]);
         if (!program.Run())
         {
